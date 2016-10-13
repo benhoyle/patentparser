8e91be4776c931dae99d12c9879b7afaf6a51110
Ben Hoyle <benjhoyle@gmail.com>
2016-10-13 10:38:09 +0100

    Working test stubs

--- frontend/public/scripts/parserfe.js
+++ frontend/public/scripts/parserfe.js
@@ -1,5 +1,39 @@
 /* Parser Front End - Javascript  File */
 
+/* Test Data */
+var words = [
+    {
+        id: 1,
+        pos: "DT",
+        word: "A"
+    },
+    {
+        id: 2,
+        pos: "NN",
+        word: "decoding"
+    },
+    {
+        id: 3,
+        pos: "NN",
+        word: "system"
+    },
+    {
+        id: 4,
+        pos: ",",
+        word: ","
+    },
+    {
+        id: 5,
+        pos: "VBG",
+        word: "comprising"
+    },
+    {
+        id: 6,
+        pos: ":",
+        word: ":"
+    }];
+
+
 var SearchForm = React.createClass({
     handleSubmit: function(e) {
         e.preventDefault();
@@ -10,8 +44,8 @@ var SearchForm = React.createClass({
       <form className="searchForm form-inline" onSubmit={this.handleSubmit}>
         <div className="form-group">Enter patent publication number to search:</div>
         <div className="form-group">
-            <label className="sr-only" for="patentNumber">Patent Publication Number</label>
-            <input type="text" class="form-control" id="patentNumber" 
+            <label className="sr-only" htmlFor="patentNumber">Patent Publication Number</label>
+            <input type="text" className="form-control" id="patentNumber" 
                 placeholder="e.g. EP3029606"
                 /*value={this.state.patentNumber}
                 onChange={this.handleNumberChange}*/
@@ -23,7 +57,63 @@ var SearchForm = React.createClass({
   }
 });
 
+var ClaimBox = React.createClass({
+    
+    render: function() {
+        return (
+            <div className="claimBox">
+                <Claim words={this.props.words}/>
+            </div>
+        );
+    }
+});
+
+var Claim = React.createClass({
+    
+    render: function() {
+        var wordNodes = this.props.words.map(function(word) {
+            return (
+                    <Word text={word.word} pos={word.pos} key={word.id}/>
+            );
+        });
+        return (
+            <div className="claim row">
+                {wordNodes}
+            </div>
+        );
+    }
+});
+
+var Word = React.createClass({
+    
+    render: function() {
+        return (
+            <div className="word col-md-1">
+                <div className="row text-center lead">
+                    <span>{this.props.text}</span>
+                </div>
+                <div className="row">
+                    <span>
+                        <select className="form-control">
+                            <option value="one">{this.props.pos}</option>
+                            <option value="two">Two</option>
+                            <option value="three">Three</option>
+                            <option value="four">Four</option>
+                            <option value="five">Five</option>
+                        </select>
+                    </span>
+                </div>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
   <SearchForm/>,
   document.getElementById('searchbar')
+);
+
+ReactDOM.render(
+  <ClaimBox words={words}/>,
+  document.getElementById('content')
 );
